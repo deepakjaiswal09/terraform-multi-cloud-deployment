0325c8a66a6cf257642590544435ca46635adae8
deepakjwll <deepakjaiswal9238@gmail.com>
2026-04-28 15:32:26 +0530

    m

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
   backend "s3" {
-     bucket = "bucket-tf-state-244-22"
+     bucket = "bucket-tf-state-242"
      key    = "terraform.tfstate"
      region = "eu-north-1"
    }
